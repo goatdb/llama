5cc34f7900e8eee18cfac7c65c1b3a8096e6da26
Peter Macko <pmacko@eecs.harvard.edu>
2015-03-04 19:51:33 -0500

    Implemented K-exposure. Minor fixes.

--- applications/sloth-weibo/sloth-weibo.cc
+++ applications/sloth-weibo/sloth-weibo.cc
@@ -44,7 +44,7 @@
 
 
 //==========================================================================//
-// SLOTH Weibo Applications                                                 //
+// Applications - Common                                                    //
 //==========================================================================//
 
 /**
@@ -54,14 +54,29 @@ class sloth_weibo_config {
 
 public:
 
+	// Streaming options
+	
 	sloth_config sloth;
 	bool verbose;
 	bool print_results;
 
+
+	// Common options for algorithms
+	
+	size_t top_n;
+
+
+	// TunkRank options
+	
 	double tr_retweet_probability;
 	double tr_diff_threshold;
 	size_t tr_max_iterations;
-	size_t tr_top_n;
+
+
+	// K-Exposure options
+	
+	size_t ke_min_count;
+	size_t ke_min_k;
 
 
 	/**
@@ -72,10 +87,14 @@ public:
 		verbose = false;
 		print_results = true;
 
+		top_n = 10;
+
 		tr_retweet_probability = 0.05;
 		tr_diff_threshold = 1e-10;
 		tr_max_iterations = 10;
-		tr_top_n = 10;
+
+		ke_min_count = 20;
+		ke_min_k = 10;
 	}
 };
 
@@ -87,8 +106,8 @@ class sloth_weibo_application : public sloth_application<tweet_t> {
 
 	typedef std::unordered_map<std::string, node_t> string_to_node_map_t;
 
-	std::vector<std::string> _users;
-	string_to_node_map_t _users_to_nodes;
+	std::vector<std::string> _names;
+	string_to_node_map_t _names_to_nodes;
 	node_t _next_available_node;
 
 
@@ -140,33 +159,33 @@ public:
 
 
 	/**
-	 * Get the number of users
+	 * Get the number of nodes
 	 *
-	 * @return the number of users
+	 * @return the number of nodes
 	 */
-	size_t num_users() {
-		return _users.size();
+	size_t num_nodes() {
+		return _names.size();
 	}
 
 
 protected:
 
 	/**
-	 * Translate user ID to the corresponding node ID. Create a new node if it
-	 * does not already exists.
+	 * Translate node name to the corresponding node ID. Create a new node if
+	 * it does not already exists.
 	 *
-	 * @param user the user name
-	 * @return the user node ID
+	 * @param name the node name
+	 * @return the node ID
 	 */
-	node_t translate_user_name(const std::string& user) {
+	node_t lookup_or_create_node(const std::string& name) {
 
-		string_to_node_map_t::iterator i = _users_to_nodes.find(user);
+		string_to_node_map_t::iterator i = _names_to_nodes.find(name);
 
-		if (i == _users_to_nodes.end()) {
+		if (i == _names_to_nodes.end()) {
 			node_t n = _next_available_node++;
-			_users_to_nodes[user] = n;
-			_users.push_back(user);
-			assert(_users.size() == _next_available_node);
+			_names_to_nodes[name] = n;
+			_names.push_back(name);
+			assert((ssize_t) _names.size() == _next_available_node);
 			return n;
 		}
 
@@ -175,14 +194,14 @@ protected:
 
 
 	/**
-	 * Get user by node ID
+	 * Get node by node ID
 	 *
 	 * @param node the node ID
-	 * @return the user name, or NULL if out of range
+	 * @return the node name, or NULL if out of range
 	 */
-	const char* user(node_t node) {
-		if (node < 0 || node >= (ssize_t) _users.size()) return NULL;
-		return _users[node].c_str();
+	const char* node_name(node_t node) {
+		if (node < 0 || node >= (ssize_t) _names.size()) return NULL;
+		return _names[node].c_str();
 	}
 
 
@@ -210,21 +229,24 @@ protected:
 			if (*start != 'u') continue;
 
 			if (sender == LL_NIL_NODE) {
-				sender = translate_user_name(std::string(tweet.t_user));
+				sender = lookup_or_create_node(std::string(tweet.t_user));
 			}
 
-			add_edge(sender, translate_user_name(user));
-			//fprintf(stderr, "%s\n", user.c_str());
+			add_edge(sender, lookup_or_create_node(user));
 		}
 	}
 };
 
 
 
+//==========================================================================//
+// Application: TunkRank (user ranking)                                     //
+//==========================================================================//
+
 /**
  * Application: User ranking
  */
-class swa_user_ranking : public sloth_weibo_application {
+class swa_tunk_rank : public sloth_weibo_application {
 
 	float* _tunk_rank;
 	size_t _tunk_rank_n;
@@ -246,19 +268,19 @@ class swa_user_ranking : public sloth_weibo_application {
 public:
 
 	/**
-	 * Create an instance of class swa_user_ranking 
+	 * Create an instance of class swa_tunk_rank 
 	 *
 	 * @param data_source the generic data source
 	 * @param config the configuration
 	 */
-	swa_user_ranking(ll_generic_data_source<tweet_t>* data_source,
+	swa_tunk_rank(ll_generic_data_source<tweet_t>* data_source,
 			const sloth_weibo_config* config)
 		: sloth_weibo_application(data_source, config) {
 	
 		_tunk_rank = NULL;
 		_tunk_rank_n = 0;
 
-		_top = (top_user*) malloc(sizeof(top_user) * _config.tr_top_n);
+		_top = (top_user*) malloc(sizeof(top_user) * _config.top_n);
 		_top_n = 0;
 	}
 
@@ -266,7 +288,7 @@ public:
 	/**
 	 * Destroy the application
 	 */
-	virtual ~swa_user_ranking() {
+	virtual ~swa_tunk_rank() {
 
 		if (_tunk_rank) free(_tunk_rank);
 		if (_top) free(_top);
@@ -281,8 +303,8 @@ public:
 	virtual void print_results(FILE* file) {
 
 		for (size_t i = 0; i < _top_n; i++) {
-			fprintf(file, "%2lu: %s (%0.6lf)\n",
-					i, user(_top[i].tu_index), _top[i].tu_score);
+			fprintf(file, "%4lu: %s (%0.6lf)\n", i+1,
+					node_name(_top[i].tu_index), _top[i].tu_score);
 		}
 	}
 
@@ -382,7 +404,7 @@ protected:
 
 		// Find the top N users
 
-		_top_n = std::min(N, _config.tr_top_n);
+		_top_n = std::min(N, _config.top_n);
 		if (_top_n > 0) {
 
 			for (size_t i = 0; i < _top_n; i++) {
@@ -408,6 +430,315 @@ protected:
 					}
 				}
 			}
+		}
+	}
+};
+
+
+
+//==========================================================================//
+// Application: K-Exposure (controversial hashtag detection)                //
+//==========================================================================//
+
+/**
+ * Application: K-Exposure (controversial hashtag detection)
+ */
+class swa_k_exposure : public sloth_weibo_application {
+
+	ll_mlcsr_edge_property<uint32_t>* _k;
+	size_t _num_hashtags;
+
+	float* _tunk_rank;
+	size_t _tunk_rank_n;
+
+	struct top_hashtag {
+
+		unsigned th_index;
+		float th_score;
+		unsigned th_count;
+		unsigned th_max_k;
+
+		bool operator() (const top_hashtag& a, const top_hashtag& b) {
+			return a.th_score > b.th_score;
+		}
+	};
+
+	top_hashtag* _top;
+	size_t _top_n;
+
+
+public:
+
+	/**
+	 * Create an instance of class swa_k_exposure 
+	 *
+	 * @param data_source the generic data source
+	 * @param config the configuration
+	 */
+	swa_k_exposure(ll_generic_data_source<tweet_t>* data_source,
+			const sloth_weibo_config* config)
+		: sloth_weibo_application(data_source, config) {
+	
+		_k = NULL;
+		_num_hashtags = 0;
+
+		_tunk_rank = NULL;
+		_tunk_rank_n = 0;
+
+		_top = (top_hashtag*) malloc(sizeof(top_hashtag) * _config.top_n);
+		_top_n = 0;
+	}
+
+
+	/**
+	 * Destroy the application
+	 */
+	virtual ~swa_k_exposure() {
+
+		if (_tunk_rank) free(_tunk_rank);
+		if (_top) free(_top);
+	}
+
+
+	/**
+	 * Print the results
+	 *
+	 * @param file the output file
+	 */
+	virtual void print_results(FILE* file) {
+
+		for (size_t i = 0; i < _top_n; i++) {
+			top_hashtag& t = _top[i];
+			fprintf(file, "%4lu: %s (F=%0.3f, N=%u, K=%u)\n", i+1,
+					node_name(t.th_index), t.th_score, t.th_count, t.th_max_k);
+		}
+	}
+
+
+protected:
+
+	/**
+	 * Custom graph initialization
+	 *
+	 * @param W the writable graph
+	 */
+	virtual void on_initialize(ll_writable_graph& W) {
+		_k = W.ro_graph().create_uninitialized_edge_property_32("k", LL_T_INT32);
+	}
+
+
+	/**
+	 * Processs an input item and generate corresponding edges
+	 *
+	 * @param input the input item
+	 */
+	virtual void on_input(const tweet_t* input) {
+
+		// Extract the "mentions" graph
+
+		extract_mentions_edges(*input);
+
+
+		// Get the hashtags
+
+		node_t user = LL_NIL_NODE;
+
+		for (const char* s = input->t_text; *s != '\0'; s++) {
+			if (*s != '#') continue;
+			
+			const char* start = s++;
+			while (*s != '\0' && *s != '#' && !isspace(*s)) s++;
+			const char* end = s;
+
+			if (*s != '#') {
+				//LL_W_PRINT("Unterminated Weibo hashtag in tweet: %s\n",
+						//input->t_text);
+				continue;
+			}
+
+			std::string hashtag = std::string(start, (long) end - (long) start);
+
+			if (user == LL_NIL_NODE) {
+				user = lookup_or_create_node(std::string(input->t_user));
+			}
+
+			add_edge(lookup_or_create_node(hashtag), user);
+			_num_hashtags++;
+		}
+	}
+
+
+	/**
+	 * Run the computation when the sliding window advances
+	 *
+	 * @param G the graph
+	 */
+	virtual void on_advance(ll_mlcsr_ro_graph& G) {
+
+		// Compute "k" edge value for each new post represented as an edge
+		// [hashtag] --> [user]
+
+
+		// For each hashtag with new posts...
+
+		ll_foreach_node_within_level_omp(n, G.out(), G.max_level(), 10240) {
+
+			// First of all, is this a hashtag? If not, skip.
+
+			if (*node_name(n) != '#') continue;
+
+
+			// Get the sorted list of users who posted with the hashtag
+
+			std::vector<node_t> posters;
+			ll_foreach_out(u, G, n) posters.push_back(u);
+			std::sort(posters.begin(), posters.end());
+
+
+			// For each new post by user u...
+
+			ll_foreach_edge_within_level(e, u, G.out(), n, G.max_level()) {
+
+				// Count the users that u pays attention to that also posted
+				// with the given hashtag n
+
+				uint32_t k = 0;
+				ll_foreach_out(t, G, u) {
+					if (std::binary_search(posters.begin(), posters.end(), t)){
+						k++;
+					}
+				}
+
+				_k->cow_write(e, k);
+			}
+		}
+
+
+		// Now compute the k histogram parameters for each hashtag
+		
+		// The minimum k to consider
+		uint32_t min_k = 0;
+
+		// The per-thread top table
+		std::vector<top_hashtag> per_thread_top[omp_get_max_threads()];
+
+		#pragma omp parallel
+		{
+			std::vector<top_hashtag>& top = per_thread_top[omp_get_thread_num()];
+
+			#pragma omp for schedule(dynamic,4096)
+			ll_foreach_node(n, G) {
+
+				// Skip non-hashtags
+
+				if (*node_name(n) != '#') continue;
+
+
+				// Create a histogram of the k's
+
+				std::vector<uint32_t> h;
+				uint32_t count = 0;
+
+				ll_foreach_out_ext(e, u, G, n) {
+					(void) u;
+					count++;
+
+					uint32_t k = _k->get(e);
+					while (h.size() <= k) h.push_back(0);
+					h[k]++;
+				}
+
+
+				// Skip histograms that do not have any non-zero k's
+
+				if (h.size() <= 1 && count > 0) continue;
+
+
+				// Compute a few helper statistics
+
+				uint32_t max_k = h.size() - 1;
+				uint32_t max_v = 0;
+
+				for (size_t i = min_k; i < h.size(); i++) {
+					max_v = std::max(max_v, h[i]);
+				}
+
+				double max_p = max_v / (double) count;
+
+
+				// Filter based on the configuration
+
+				if (count < _config.ke_min_count) continue;
+				if (max_k < _config.ke_min_k    ) continue;
+
+
+				// Compute the persistence parameter
+
+				double A = 0;
+				for (size_t i = min_k; i < h.size() - 1; i++) {
+					A += (h[i] + h[i+1]) / (double) count / 2;
+				}
+
+				double R = max_k * max_p;
+				double F = A / R;
+
+
+				// Add the per-thread top table
+
+				top_hashtag th;
+				th.th_index = n;
+				th.th_score = F;
+				th.th_max_k = max_k;
+				th.th_count = count;
+
+				if (top.size() < _config.top_n) {
+
+					top.push_back(th);
+
+					if (top.size() == _config.top_n) {
+						std::sort(top.begin(), top.end(), th);
+					}
+				}
+				else if (F > top[top.size()-1].th_score) {
+
+					top[top.size()-1] = th;
+
+					for (size_t j = top.size()-1; j > 0; j--) {
+						if (top[j-1].th_score < top[j].th_score) {
+							top_hashtag tmp = top[j-1];
+							top[j-1] = top[j];
+							top[j] = tmp;
+						}
+						else {
+							break;
+						}
+					}
+				}
+
+				//if (--c >= 0) {
+					//LL_D_PRINT("%ld %s --> %lu\n", n, node_name(n), h.size());
+				//}
+			}
+		}
+
+
+		// Merge the top tables
+
+		std::vector<top_hashtag> top;
+		for (int thread = 0; thread < omp_get_max_threads(); thread++) {
+			for (size_t i = 0; i < per_thread_top[thread].size(); i++) {
+				top.push_back(per_thread_top[thread][i]);
+			}
+		}
+
+		top_hashtag th;
+		memset(&th, 0, sizeof(th));
+
+		std::sort(top.begin(), top.end(), th);
+
+		_top_n = std::min(_config.top_n, top.size());
+		for (size_t i = 0; i < _top_n; i++) {
+			_top[i] = top[i];
 		}
 	}
 };
@@ -529,18 +860,18 @@ public:
 		}
 
 		fprintf(_progress_file, "%s"
-				"%6.3lf M tweets/s, %6.3lf M edges/s, "
+				"%5.0lf K tweets/s, %5.0lf K edges/s, "
 				"%6.3lf s adv, %6.3lf s run, "
-				"%6.3lf GB MaxRSS, %6.3lf M users%s\n",
+				"%6.3lf GB MaxRSS, %6.3lf M nodes%s\n",
 				_progress_tty ? LL_C_B_BLUE : "",
 				_application->last_batch_inputs()
-					/ driver.last_interval_time_ms() / 1000.0,
+					/ driver.last_interval_time_ms(),
 				driver.last_batch_requests()
-					/ driver.last_interval_time_ms() / 1000.0,
+					/ driver.last_interval_time_ms(),
 				driver.last_advance_time_ms() / 1000.0,
 				driver.last_compute_time_ms() / 1000.0,
 				ll_get_maxrss_kb() / 1048576.0,
-				_application->num_users() / 1000000.0,
+				_application->num_nodes() / 1000000.0,
 				_progress_tty ? LL_C_RESET : "");
 		fflush(_progress_file);
 
@@ -558,7 +889,28 @@ public:
 	 *
 	 * @param ms the number of ms we are behind the schedule
 	 */
-	virtual void behind(double ms) {}
+	virtual void behind(double ms) {
+
+		ll_sliding_window_driver& driver = _application->driver();
+
+		if (_progress_tty) {
+			fprintf(_progress_file, "\r%s%4ld: ",
+					_progress_tty ? LL_C_B_BLUE : "",
+					driver.batch());
+		}
+
+		ssize_t t = driver.window_config().swc_advance_interval_ms;
+		double p = t <= 0 ? 0 : 100.0 * ms / driver.last_interval_time_ms();
+
+		fprintf(_progress_file, "%s%6.3lf s behind",
+				_progress_tty ? LL_C_B_YELLOW : "",
+				ms / 1000.0);
+		if (t > 0) fprintf(_progress_file, " (%6.2lf%%)", p);
+
+		fprintf(_progress_file, "%s\n", _progress_tty ? LL_C_RESET : "");
+		fflush(_progress_file);
+	}
+
 
 
 	/**
@@ -585,7 +937,7 @@ static struct option LONG_OPTIONS[] =
 {
 	{"advance"      , required_argument, 0, 'A'},
 	{"help"         , no_argument      , 0, 'h'},
-	{"max-batches"  , required_argument, 0, 'M'},
+	{"max-advances" , required_argument, 0, 'M'},
 	{"rate"         , required_argument, 0, 'R'},
 	{"run"          , required_argument, 0, 'r'},
 	{"silent"       , no_argument      , 0, 's'},
@@ -610,7 +962,7 @@ static void usage(const char* arg0) {
 	fprintf(stderr, "Options:\n");
 	fprintf(stderr, "  -A, --advance N       Set the advance interval (seconds)\n");
 	fprintf(stderr, "  -h, --help            Show this usage information and exit\n");
-	fprintf(stderr, "  -M, --max-batches N   Set the maximum number of batches\n");
+	fprintf(stderr, "  -M, --max-advances N  Set the maximum number of advances\n");
 	fprintf(stderr, "  -R, --rate N          Set the max stream rate (edges/second)\n");
 	fprintf(stderr, "  -r, --run APP         Run the given application\n");
 	fprintf(stderr, "  -s, --silent          Silent mode (do not print results)\n");
@@ -734,10 +1086,14 @@ int main(int argc, char** argv)
 	sloth_weibo_application* application = NULL;
 
 	if (s_application == NULL) {
-		application = new swa_user_ranking(&data_source, &config);
+		application = new swa_tunk_rank(&data_source, &config);
 	}
-	else if (strcmp(s_application, "ur") == 0) {
-		application = new swa_user_ranking(&data_source, &config);
+	else if (strcmp(s_application, "tunkrank") == 0) {
+		application = new swa_tunk_rank(&data_source, &config);
+	}
+	else if (strcmp(s_application, "k-exposure") == 0
+			|| strcmp(s_application, "k-exp") == 0) {
+		application = new swa_k_exposure(&data_source, &config);
 	}
 	else {
 		fprintf(stderr, "Error: Unknown application \"%s\"\n", s_application);
